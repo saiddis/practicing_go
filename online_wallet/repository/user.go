246706dc8810e52a0920869c8883da8b31ddd17f
saiddis <saiddisruptive@gmail.com>
2024-11-06 16:47:35 +0500

    repository update

--- online_wallet/repository/user.go
+++ online_wallet/repository/user.go
@@ -15,22 +15,30 @@ type UserRepository struct {
 	*postgres.PG
 }
 
+func NewUserRepository(db *postgres.PG) *UserRepository {
+	return &UserRepository{
+		PG: db,
+	}
+}
+
 func (us UserRepository) FindUserByID(ctx context.Context, id int) (*domain.User, error) {
 	var user domain.User
-	err := us.DB.QueryRow(ctx, "select * from users where id = $1;", id).Scan(&user)
+	row := us.DB.QueryRow(ctx, "SELECT * FROM users WHERE id = $1;", id)
+
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt, &user.UpdatedAt, &user.Balance)
 	if err != nil {
-		log.Printf("QueryRow failed: %v", err)
+		log.Printf("Error retrieving user by id: %v", err)
 	}
 	return &user, nil
 }
 
 func (us *UserRepository) CreateUser(ctx context.Context, user domain.User) error {
-	query := `INSERT INTO users (name, email) VALUES (
+	query := `INSERT INTO users (name, email, created_at, updated_at, balance) VALUES (
 	@name,
 	@email,
 	@createdAt,
 	@updatedAt,
-	@balace
+	@balance
 	);`
 	args := pgx.NamedArgs{
 		"name":      user.Name,
@@ -40,10 +48,11 @@ func (us *UserRepository) CreateUser(ctx context.Context, user domain.User) erro
 		"balance":   user.Balance,
 	}
 
-	_, err := us.DB.Exec(ctx, query, args)
+	tag, err := us.DB.Exec(ctx, query, args)
 	if err != nil {
 		return fmt.Errorf("error inserting row: %v", err)
 	}
+	log.Printf("Create user: %v", tag)
 	return nil
 }
 
@@ -59,10 +68,11 @@ func (us *UserRepository) UpdateUser(ctx context.Context, id int, upd domain.Use
 		"updatedAt": time.Now().UTC(),
 	}
 
-	_, err := us.DB.Exec(ctx, query, args)
+	tag, err := us.DB.Exec(ctx, query, args)
 	if err != nil {
 		return fmt.Errorf("error updating record: %v", err)
 	}
+	log.Printf("Update user with id %d: %v", id, tag)
 	return nil
 }
 
@@ -72,14 +82,15 @@ func (us *UserRepository) DeleteUser(ctx context.Context, id int) error {
 	args := pgx.NamedArgs{
 		"id": id,
 	}
-	_, err := us.DB.Exec(ctx, query, args)
+	tag, err := us.DB.Exec(ctx, query, args)
 	if err != nil {
 		return fmt.Errorf("error deleting record: %v", err)
 	}
+	log.Printf("Delete user with id %d: %v", id, tag)
 	return nil
 }
 
-func (us *UserRepository) Transfer(ctx context.Context, srcID, dstID int, amount float64) error {
+func (us *UserRepository) Transfer(ctx context.Context, srcID, dstID int, amount float32) error {
 	conn, err := us.DB.Acquire(ctx)
 	if err != nil {
 		return domain.Errorf(domain.EINTERNAL, "error acquiring connection: %v", err)
@@ -112,40 +123,14 @@ func (us *UserRepository) Transfer(ctx context.Context, srcID, dstID int, amount
 	return nil
 }
 
-func (us *UserRepository) Credit(ctx context.Context, id int, amount float64) error {
+func (us *UserRepository) Credit(ctx context.Context, id int, amount float32) error {
 	user, err := us.FindUserByID(ctx, id)
 	if err != nil {
-		return domain.Errorf(domain.EINVALID, "error retrieving user with id:%d: %v", id, err)
+		return domain.Errorf(domain.EINVALID, "error retrieving user with id %d: %v", id, err)
 	}
 	newBalance := user.Balance + amount
 	if newBalance > domain.UnauthorizedBalanceLimit {
-		return domain.Errorf(domain.EREACHEDLIMIT, "error adding up user's balance with id:%d", user.ID)
-	}
-	query := `UPDATE users 
-	SET balance = balance, updated_at = @updatedAt
-	WHERE id = @id;
-	`
-	args := pgx.NamedArgs{
-		"id":        id,
-		"balance":   newBalance,
-		"updatedAt": time.Now().UTC(),
-	}
-
-	_, err = us.DB.Exec(ctx, query, args)
-	if err != nil {
-		return domain.Errorf(domain.EINTERNAL, "error crediting user's balance: %v", err)
-	}
-	return nil
-}
-
-func (us *UserRepository) Withdraw(ctx context.Context, id int, amount float64) error {
-	user, err := us.FindUserByID(ctx, id)
-	if err != nil {
-		return domain.Errorf(domain.EINVALID, "error retrieving user with id:%d: %v", id, err)
-	}
-	newBalance := user.Balance - amount
-	if newBalance < 0 {
-		return domain.Errorf(domain.ENOTENOUGH, "error withdrawing from user with id:%d", user.ID)
+		return domain.Errorf(domain.EREACHEDLIMIT, "error adding up user's balance with id %d", user.ID)
 	}
 	query := `UPDATE users 
 	SET balance = @balance, updated_at = @updatedAt
@@ -157,9 +142,37 @@ func (us *UserRepository) Withdraw(ctx context.Context, id int, amount float64) 
 		"updatedAt": time.Now().UTC(),
 	}
 
-	_, err = us.DB.Exec(ctx, query, args)
+	tag, err := us.DB.Exec(ctx, query, args)
 	if err != nil {
 		return domain.Errorf(domain.EINTERNAL, "error crediting user's balance: %v", err)
 	}
+	log.Printf("Credit to user with id %d: %v", id, tag)
+	return nil
+}
+
+func (us *UserRepository) Withdraw(ctx context.Context, id int, amount float32) error {
+	user, err := us.FindUserByID(ctx, id)
+	if err != nil {
+		return domain.Errorf(domain.EINVALID, "error retrieving user with id %d: %v", id, err)
+	}
+	newBalance := user.Balance - amount
+	if newBalance < 0 {
+		return domain.Errorf(domain.ENOTENOUGH, "error withdrawing from user with id %d", user.ID)
+	}
+	query := `UPDATE users 
+	SET balance = @balance, updated_at = @updatedAt
+	WHERE id = @id;
+	`
+	args := pgx.NamedArgs{
+		"id":        id,
+		"balance":   newBalance,
+		"updatedAt": time.Now().UTC(),
+	}
+
+	tag, err := us.DB.Exec(ctx, query, args)
+	if err != nil {
+		return domain.Errorf(domain.EINTERNAL, "error crediting user's balance: %v", err)
+	}
+	log.Printf("Withdraw from user with id %d: %v", id, tag)
 	return nil
 }
